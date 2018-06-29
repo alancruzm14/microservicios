da8c1ea8697d2f2149905c9e380bac3b4e1977aa
ajcruzmi <ajcruzmi@ekt1369090mac.local>
2018-06-29 13:36:47 -0500

    Creacion del controlador(Orquestador)

---
+++ BDM-Biometricos-Controller/src/main/java/com/bancoazteca/controller/Application.java
@@ -0,0 +1,26 @@
+package com.bancoazteca.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+@ComponentScan("com.bz.spring.*")
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		AopAutoConfiguration.class
+} )
+public class Application {
+
+    public Application(){
+        //For Spring
+    	
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
